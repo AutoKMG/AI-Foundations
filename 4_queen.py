cbff40ea01ed695c9c52a8edefab59d879097e0d
Khaled Saleh <khaled@connou.app>
2023-03-19 17:52:26 +0100

    writing the result and goal_test methods

--- 4_queen.py
+++ 4_queen.py
@@ -5,7 +5,7 @@ def convert_state_to_list(state):
     return [list(x) for x in state]
 
 
-def covert_list_to_state(state):
+def convert_list_to_state(state):
     return tuple([tuple(x) for x in state])
 class FourQueensProblem(Problem):
     def __init__(self):
@@ -30,12 +30,25 @@ class FourQueensProblem(Problem):
     def result(self, state, action):
         # Return with the new state of the result of the action parameter used in the state parameter.
         # Tip: don't forget to convert state to list of lists and then convert the result back to tuple of tuples
-        pass
+        i = int(action.split(' ')[1])
+        j = int(action.split(' ')[2])
+        new_state = convert_state_to_list(state)
+        for r in range(4):
+            for c in range(4):
+                if r == i and c == j:
+                    new_state[i][j] = 1
+                elif (r == i or c == j or abs(i - r) == abs (j - c)) and not (r == i and c == j):
+                    new_state[i][j] = 2
+        return convert_list_to_state(new_state)
 
     def goal_test(self, state):
         # For a given state parameter check if it is a goal state.
         # Tip 1: don't forget conversions; Tip 2: you can use any() or all() for easier implementation
-        pass
+        bool_state = convert_state_to_list(state)
+        for i in range(4):
+            for j in range(4):
+                bool_state[i][j] = state[i][j] == 1
+        return all([any(bool_state[i]) for i in range(4)])
 
 
 def main():
